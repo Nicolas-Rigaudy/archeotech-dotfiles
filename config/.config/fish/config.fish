20c7c085ccd7e7264a391df7e23a93e741290bdc
Nicolas Rigaudy <nicolas.rigaudy@gmail.com>
2025-12-01 18:54:03 +0100

    new[CONF]: complete stow-based dotfiles setup

---
+++ config/.config/fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	starship init fish | source
+	zoxide init fish --cmd cd | source
+	thefuck --alias | source
+	alias ls "exa -al --icons"
+	alias cat "bat"
+	fastfetch
+end
+
+# Add local bin to PATH
+export PATH="$HOME/.local/bin:$PATH"
+alias cp /usr/local/bin/advcp -g
+alias mv /usr/local/bin/advmv -g
